7bf901c63ddfe2ac30236429fa4aca09fc371f5b
jfredrickson <jeff.fredrickson@gmail.com>
2013-11-26 10:12:19 -0500

    initial import of site template

---
+++ js/global.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    var sections = $('section');
+    var navLinks = $('#topics-link, #speakers-link, #sponsors-link');
+
+    sections.waypoint({
+        handler: function (direction) {
+            if (direction === 'up') {
+                var currentSection = $(this);
+                navLinks.removeClass('current-section');
+                $('nav a#' + currentSection.attr('id') + '-link').addClass('current-section');
+            }
+        },
+        offset: function () { return -($(this).height() / 2); }
+    });
+
+    sections.waypoint({
+        handler: function (direction) {
+            if (direction === 'down') {
+                var currentSection = $(this);
+                navLinks.removeClass('current-section');
+                $('nav a#' + currentSection.attr('id') + '-link').addClass('current-section');
+            }
+        },
+        offset: '200px'
+    });
+
+    navLinks.click(function (event) {
+        var clickedLinkId = $(this).attr('id');
+        var target = clickedLinkId.substr(0, clickedLinkId.length - 5);
+        $('html, body').animate({
+            scrollTop: $('#' + target).offset().top - 60
+        }, 400);
+        event.preventDefault();
+    });
+
+    $('.title a').click(function (event) {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 400);
+        event.preventDefault();
+    });
+});
